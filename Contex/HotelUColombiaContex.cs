835e95335ba25edf320f3d9ca27a037c1feae79b
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2023-09-11 10:56:27 -0500

    Preparando proyecto para DB

---
+++ Contex/HotelUColombiaContex.cs
@@ -0,0 +1,21 @@
+﻿using HotelUColombia.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class HotelUColombiaContext : DbContext
+    {
+        public DbSet<Booking> Booking { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Rooms> Room { get; set; }
+        public DbSet<StatusBooking> StatusBooking { get; set; }
+        public DbSet<User> User { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
