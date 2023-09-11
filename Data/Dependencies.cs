4aa7cfbf05bd3267cd000c3b80ee4c49c04507d0
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2023-09-11 16:25:47 -0500

    crud : booking and statusbooking

--- Data/Dependencies.cs
+++ Data/Dependencies.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Configuration;
-using HotelUColombia.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace HotelUColombia.Data;
 public static class Dependencies
 {
-    public static void ConfigureServices(IConfiguration configuration, IServiceCollection services) 
-    => services.AddDbContext<HotelUColombiaContext>(c =>c.UseNpgsql(configuration.GetConnectionString("HotelUColombiaContext")));
-    
+    public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+    => services.AddDbContext<HotelUColombiaContext>(c => c.UseNpgsql(configuration.GetConnectionString("HotelUColombiaContext")));
+
 }
