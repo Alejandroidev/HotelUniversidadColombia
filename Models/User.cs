f01f86663315928ede77c8c6376aa4e133faa6f4
KatheJacobo <katejacobopere@gmail.com>
2023-09-11 15:32:02 -0500

    Add propieties Apellido,Rol, Correo

--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,20 @@
         /// </summary>
         public string Nombre { get; set; }
 
+        /// <summary>
+        /// Get or Set Apellido
+        /// </summary>
+        public string Apellido { get; set; }
+
+        /// <summary>
+        /// Get or Set Rol
+        /// </summary>
+        public string Rol { get; set; }
+
+        /// <summary>
+        /// Get or Set Correo
+        /// </summary>
+        public string Correo { get; set; }
+
     }
 }
