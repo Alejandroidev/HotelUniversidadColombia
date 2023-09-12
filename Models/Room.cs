1f2b665f464f06f12191a0fa6ad06b67b015e103
Esca84 <144290510+Esca84@users.noreply.github.com>
2023-09-12 18:05:15 -0500

    add prop
    agregamos propiedades a roms

--- Models/Room.cs
+++ Models/Room.cs
@@ -2,5 +2,14 @@
 {
     public class Rooms : BaseClass
     {
+        /// <summary>
+        /// Get or Set Categoria
+        /// </summary>
+        public string Categoria { get; set; }
+
+        /// <summary>
+        /// Get or set TV
+        /// </summary>
+        public bool TV { get; set; }
     }
 }
