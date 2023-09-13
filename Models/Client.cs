a9acf58d3d94dfe8d120619803719b81ea677159
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2023-09-13 17:53:52 -0500

    modelo cliente

--- Models/Client.cs
+++ Models/Client.cs
@@ -2,5 +2,25 @@
 {
     public class Client : BaseClass
     {
+        /// <summary>
+        /// Get or Set Name
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// Get or Set last name
+        /// </summary>
+        public string? LastName { get; set; }
+        /// <summary>
+        /// Get or Set phone
+        /// </summary>
+        public string? Phone { get; set; }
+        /// <summary>
+        /// Get or Set Email
+        /// </summary>
+        public string? Email { get; set; }
+        /// <summary>
+        /// Get or Set Coments
+        /// </summary>
+        public string? Coments { get; set; }
     }
 }
