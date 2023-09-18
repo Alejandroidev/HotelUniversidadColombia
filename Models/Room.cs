9f60650c5deecffdebeb764f6675425a65c934cd
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2023-09-18 06:34:53 -0500

    croom

--- Models/Room.cs
+++ Models/Room.cs
@@ -2,5 +2,11 @@
 {
     public class Rooms : BaseClass
     {
+        public string Category { get; set; }
+        public bool TV { get; set; }
+        public bool Bathroom { get; set; }
+        public bool Freezer { get; set; }
+        public int NumberRooms { get; set; }
+
     }
 }
