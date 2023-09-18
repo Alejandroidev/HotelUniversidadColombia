d3aa0af2858e46f60431f5819a91d04791711233
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2023-09-18 18:03:55 -0500

    rooms

---
+++ Controllers/RoomsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HotelUColombia.Data;
+using HotelUColombia.Models;
+
+namespace HotelUColombia.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly HotelUColombiaContext _context;
+
+        public RoomsController(HotelUColombiaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rooms
+        public async Task<IActionResult> Index(int id)
+        {
+            if (id != 0)
+            {
+                var room = _context.Rooms.Where(room => room.Id == id);
+                return _context.Rooms.Where(room => room.Id == id) != null ?
+                View(room) :
+                Problem("Entity set 'HotelUColombiaContext.Rooms'  is null.");
+            }
+            else
+            {
+                return _context.Rooms != null ?
+                View(await _context.Rooms.ToListAsync()) :
+                Problem("Entity set 'HotelUColombiaContext.Rooms'  is null.");
+            }
+
+
+        }
+
+        // GET: Rooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var rooms = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rooms == null)
+            {
+                return NotFound();
+            }
+
+            return View(rooms);
+        }
+
+        // GET: Rooms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Category,TV,Bathroom,Freezer,NumberBeds,Image,Price,Id")] Rooms rooms)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rooms);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rooms);
+        }
+
+        // GET: Rooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var rooms = await _context.Rooms.FindAsync(id);
+            if (rooms == null)
+            {
+                return NotFound();
+            }
+            return View(rooms);
+        }
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Category,TV,Bathroom,Freezer,NumberBeds,Image,Price,Id")] Rooms rooms)
+        {
+            if (id != rooms.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rooms);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomsExists(rooms.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rooms);
+        }
+
+        // GET: Rooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var rooms = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rooms == null)
+            {
+                return NotFound();
+            }
+
+            return View(rooms);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Rooms == null)
+            {
+                return Problem("Entity set 'HotelUColombiaContext.Rooms'  is null.");
+            }
+            var rooms = await _context.Rooms.FindAsync(id);
+            if (rooms != null)
+            {
+                _context.Rooms.Remove(rooms);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomsExists(int id)
+        {
+            return (_context.Rooms?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Helper/DisponibilidadHelper.cs
+++ Helper/DisponibilidadHelper.cs
@@ -1,6 +1,28 @@
-﻿namespace HotelUColombia.Helper
+﻿using HotelUColombia.Data;
+using HotelUColombia.Models;
+
+namespace HotelUColombia.Helper
 {
     public class DisponibilidadHelper
     {
+        private readonly HotelUColombiaContext _context;
+        public DisponibilidadHelper(HotelUColombiaContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Calculate Total Price Booking
+        /// </summary>
+        /// <param name="pickUp">pick up booking</param>
+        /// <param name="returnday">return day booking</param>
+        /// <param name="priceDialy">price daily room</param>
+        /// <returns>total price booking</returns>
+        public static double GetTotalPrice(DateTime pickUp, DateTime returnday, double priceDialy) 
+        {
+            TimeSpan daysBooking = pickUp - returnday;
+            int days = daysBooking.Days;
+            return days * priceDialy;
+        }
+
     }
 }
