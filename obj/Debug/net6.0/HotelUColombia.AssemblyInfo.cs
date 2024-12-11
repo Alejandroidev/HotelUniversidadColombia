d626a60f5c2b54cc7084647cc6065fc84bf4fd5c
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2024-12-11 14:14:20 -0500

    gitignore

--- obj/Debug/net6.0/HotelUColombia.AssemblyInfo.cs
+++ obj/Debug/net6.0/HotelUColombia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelUColombia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b8fd5324ed9be7cf0edd3f751932dab43580bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9e93591a0cbd7d5ae829dcc53b35a4fb5350be")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelUColombia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelUColombia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
